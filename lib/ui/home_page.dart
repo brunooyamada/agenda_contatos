722ef01235bd9659dd8f567a670b6fe0c78add7c
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-12-09 22:27:37 -0300

    170. Criando um Botão Flutuante

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -11,25 +11,40 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final ContactHelper _helper = ContactHelper();
 
+  List<Contact> contacts = [];
+
   @override
   void initState() {
     super.initState();
 
-    Contact c = Contact();
-    c.name = "Marcos";
-    c.email = "marcos@gmail.com";
-    c.phone = "99999-9999";
-    c.img = "imgtest";
-
-    _helper.saveContact(c);
-
     _helper.getAllContacts().then((list) {
-      print(list);
+      setState(() {
+        contacts = list;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contatos'),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return null;
+        },
+      ),
+    );
   }
 }
